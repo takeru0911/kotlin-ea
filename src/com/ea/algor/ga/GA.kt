2cf4b8a730d6b3c4dd8b9e6121ea7649181306b1
takeru0911 <takeru911run@gmail.com>
2015-09-24 16:47:03 +0900

    impl all ga's operation

--- src/com/ea/algor/ga/GA.kt
+++ src/com/ea/algor/ga/GA.kt
@@ -8,25 +8,73 @@ import com.ea.prob.Problem
  * Created by taker on 2015/09/24.
  */
 
+fun makeCrossoverMask(maskLength: Int): Array<Int>{
+    val mask = Array(maskLength){
+        0
+    }
+    for(i in 0 .. maskLength - 1){
+        mask[i] = if(Math.random() > 0.5){
+            0
+        }else{
+            1
+        }
+    }
+
+    return mask
+}
+
 class GA(problem: Problem,prop: PropertyGA): Algorithm{
     var curEvaluatedIndex = -1
+    private var crossoverMask: Array<Int>? = null
 
     /**
      * uniform crossover
      */
     val crossoverOperation: (Solution, Solution) -> Solution = {
-
+        parent1, parent2 ->
+        val crossoveredVars = parent1.mergeWithIndexed(parent2, {
+            idx, v1, v2 ->
+            if (crossoverMask!![idx] == 0) {
+                v1
+            } else {
+                v2
+            }
+        })
+        Solution(crossoveredVars)
     }
 
-    fun crossover(parent1: Solution, parent2: Solution): Solution{
+    override fun crossover(parent1: Solution, parent2: Solution): Solution{
+        return crossoverOperation(parent1, parent2)
+    }
+    /*
+     * bit flip
+     */
+    val mutateOperation: (Solution) -> Solution = {
+        target ->
+        val mutatedVars = target.vars.map {
+            v ->
+            if(prop.CR > Math.random()){
+                Math.abs(v - 1)
+            }else{
+                v
+            }
+        }.toTypedArray()
 
+        Solution(mutatedVars)
+    }
+    override fun mutation(target: Solution): Solution{
+        return mutateOperation(target)
     }
 
-    fun mutation(target: Solution): Solution{
-
+    /**
+     * replace
+     */
+    val selectionOperation: (Solution, Solution) -> Solution = {
+        base, trial ->
+        trial
     }
 
-    fun selection(base: Solution, trial: Solution): Solution{
-
+    override fun selection(base: Solution, trial: Solution): Solution{
+        return selectionOperation(base, trial)
     }
 }
--- src/com/ea/algor/ga/PropertyGA.kt
+++ src/com/ea/algor/ga/PropertyGA.kt
@@ -4,9 +4,9 @@ package com.ea.algor.ga
  * Created by taker on 2015/09/24.
  */
 data class PropertyGA(
-        numOfPopulation: Int,
-        numOfDimension: Int,
-        nuOfElite: Int,
-        CR: Double,
-        mutationRate: Double
+        val numOfPopulation: Int,
+        val numOfDimension: Int,
+        val nuOfElite: Int,
+        val CR: Double,
+        val mutationRate: Double
 )
